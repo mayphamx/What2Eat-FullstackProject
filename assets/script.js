212c61d516e443c9e5ab0354b92f8a0d75814cfd
May <maypham@utexas.edu>
2023-07-14 12:14:09 -0500

    added googleplaces api to html(js)

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,8 @@
 // API keys (+backups)
 var placesKey1="AIzaSyBDr_dT0gyfPk9TNst7heS7aD12S8A2nLk";
 var tastyKey1= '66ffc7fa92msh170770be6ee18ffp15c6b3jsn88d8fc037362';
-// var placesKey2="AIzaSyATmyZ1ZmaWihFMPJLJoLEGfbzjESWfWe8";
-// var tastyKey2= '1ab6ffd471mshbf01716883afebfp1595bcjsnfab03526961a';
+var placesKey2="AIzaSyATmyZ1ZmaWihFMPJLJoLEGfbzjESWfWe8";
+var tastyKey2= '1ab6ffd471mshbf01716883afebfp1595bcjsnfab03526961a';
 
 // Tasty API - recipes/list - JS (fetch) code snippet
 var tastyApi = 'https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_30_minutes';
@@ -14,7 +14,7 @@ var tastyOptions = {
 	}
 };
 
-// Tasty API fetch
+// Tasty API fetch data
 fetch(tastyApi, tastyOptions)
     .then(function (response) {
       return response.json();
@@ -25,50 +25,53 @@ fetch(tastyApi, tastyOptions)
     })
 
 // Google Places API - search place by query 
-let map;
-let service;
-let infowindow;
+// let map;
+// let service;
+// let infowindow;
 
-function initMap() {
-  const sydney = new google.maps.LatLng(-33.867, 151.195);
+// function initMap() {
+//   const sydney = new google.maps.LatLng(-33.867, 151.195);
 
-  infowindow = new google.maps.InfoWindow();
-  map = new google.maps.Map(document.getElementById("maps"), {
-    center: sydney,
-    zoom: 15,
-  });
+//   infowindow = new google.maps.InfoWindow();
+//   map = new google.maps.Map(document.getElementById("map"), {
+//     center: sydney,
+//     zoom: 15,
+//   });
 
-  const request = {
-    query: "Museum of Contemporary Art Australia",
-    fields: ["name", "geometry"],
-  };
+//   const request = {
+//     query: "Museum of Contemporary Art Australia",
+//     fields: ["name", "geometry"],
+//   };
 
-  service = new google.maps.places.PlacesService(map);
-  service.findPlaceFromQuery(request, (results, status) => {
-    if (status === google.maps.places.PlacesServiceStatus.OK && results) {
-      for (let i = 0; i < results.length; i++) {
-        createMarker(results[i]);
-      }
+//   service = new google.maps.places.PlacesService(map);
+//   service.findPlaceFromQuery(request, (results, status) => {
+//     if (status === google.maps.places.PlacesServiceStatus.OK && results) {
+//       for (let i = 0; i < results.length; i++) {
+//         createMarker(results[i]);
+//       }
 
-      map.setCenter(results[0].geometry.location);
-    }
-  });
-}
+//       map.setCenter(results[0].geometry.location);
+//     }
+//   });
+// }
 
-function createMarker(place) {
-  if (!place.geometry || !place.geometry.location) return;
+// function createMarker(place) {
+//   if (!place.geometry || !place.geometry.location) return;
 
-  const marker = new google.maps.Marker({
-    map,
-    position: place.geometry.location,
-  });
+//   const marker = new google.maps.Marker({
+//     map,
+//     position: place.geometry.location,
+//   });
 
-  google.maps.event.addListener(marker, "click", () => {
-    infowindow.setContent(place.name || "");
-    infowindow.open(map);
-  });
-}
-window.initMap = initMap;
-initMap();
+//   google.maps.event.addListener(marker, "click", () => {
+//     infowindow.setContent(place.name || "");
+//     infowindow.open(map);
+//   });
+// }
+// window.initMap = initMap;
+// console.log("test");
+
+// // call function
+// initMap();
 
 
