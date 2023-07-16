5433e610d15ebac40302f309cc40996e576f90a0
May <maypham@utexas.edu>
2023-07-16 17:43:48 -0500

    changed food api, added for loops for data info

--- assets/script.js
+++ assets/script.js
@@ -1,47 +1,56 @@
 // API keys (+backups)
-// var placesKey1="AIzaSyBDr_dT0gyfPk9TNst7heS7aD12S8A2nLk";
-// var tastyKey1= '66ffc7fa92msh170770be6ee18ffp15c6b3jsn88d8fc037362';
-// var placesKey2="AIzaSyATmyZ1ZmaWihFMPJLJoLEGfbzjESWfWe8";
-// var tastyKey2= '1ab6ffd471mshbf01716883afebfp1595bcjsnfab03526961a';
+var placesKey1="AIzaSyBDr_dT0gyfPk9TNst7heS7aD12S8A2nLk";
+var recipeKey1= '66ffc7fa92msh170770be6ee18ffp15c6b3jsn88d8fc037362';
+var placesKey2="AIzaSyATmyZ1ZmaWihFMPJLJoLEGfbzjESWfWe8";
+var recipeKey2= '1ab6ffd471mshbf01716883afebfp1595bcjsnfab03526961a';
 
-// Tasty API - recipes/list - JS (fetch) code snippet
-// var tastyApi = 'https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_30_minutes';
-// var tastyOptions = {
-//   method: 'GET',
-// 	headers: {
-//     'X-RapidAPI-Key': '66ffc7fa92msh170770be6ee18ffp15c6b3jsn88d8fc037362',
-// 		'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
-// 	}
-// };
 
  // button & user input id -> variables
- var foodSearchButton = document.getElementById(foodSearch-btn);
- var userInput = document.getElementById(userInput);
+ var foodSearchButton = document.getElementById('foodSearch-btn');
+ var userInput = document.getElementById('userInput');
+ var recipeSearchButton = document.getElementById('recipeSearch-btn');
  
  // click event listener with function
- foodSearchButton.addeventlistener('click',getFood);
-var recipeSearchButton = document.getElementById(recipeSearch-btn);
-recipeSearchButton.addeventlistener('click',getRecipe);
+// foodSearchButton.addeventlistener('click',getFood);
+recipeSearchButton.addEventListener('click',getRecipe);
 
 function getRecipe() {
-  var inputCity = document.getElementById('userInput');
-  var tastyApi = 'https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_30_minutes';
-  var tastyOptions = {
+  // API variables using user input 
+  var userInput = document.getElementById('userInput').value;
+  var recipeApi = 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch?query='+userInput+'&instructionsRequired=true&fillIngredients=true&addRecipeInformation=true&ignorePantry=true';
+  var recipeOptions = {
     method: 'GET',
     headers: {
-      'X-RapidAPI-Key': '66ffc7fa92msh170770be6ee18ffp15c6b3jsn88d8fc037362',
-      'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
+      'X-RapidAPI-Key': '1ab6ffd471mshbf01716883afebfp1595bcjsnfab03526961a',
+      'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
     }
   };
 
-  fetch(tastyApi, tastyOptions)
+  fetch(recipeApi, recipeOptions)
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
-  console.log('Tasty Fetch Response \n-------------');
+  console.log('Recipe Data Fetch Response \n-------------');
   console.log(data);
+  // loop through data/recipe
+  for (x = 0; x < 6; x++) {
+    var title = data.results[x].title;
+    console.log('Recipe Title \n-------------');
+    console.log(title);
+    
+
+    console.log("Recipe Instructions\n-------------");
+    // loop through instruction steps per each data
+    var instructions = data.results[x].analyzedInstructions[0].steps; // returns instructions of FIRST result in a array
+    // console.log(instructions);
+    for (i = 0; i < instructions.length; i++) {
+      console.log((i+1)+"."+instructions[i].step);
+    
+    }
+  }
+    
   })
 }
 
-getRecipe();
+// getRecipe();
