17f69b930bcdc8b3953519e7cf4d6b2bfaf6f5f1
micavilla <micaella.villa1@gmail.com>
2023-07-17 15:03:44 -0500

    js for POI data

--- assets/scriptmap.js
+++ assets/scriptmap.js
@@ -1,24 +1,36 @@
 var searchBtn = document.getElementById("map-search");
-searchBtn.addEventListener('click', getPOIs);
+searchBtn.addEventListener('click', getCoor);
 
-
-function getPOIs() {
+function getCoor() {
   var userInput = document.getElementById('user-input').value;
   var address = userInput.split(' ');
   console.log(address);
-  var urlarray = "";
+  var urlArray = "";
   for (i=0; i < address.length; i++) {
-    urlarray +=(address[i]+"%20");
-    urlarray.toString();
+    urlArray +=(address[i]+"%20");
+    urlArray.toString();
   }
-  console.log(urlarray);
+  console.log(urlArray);
 
-
-fetch("https://api.tomtom.com/search/2/search/" + urlarray +".json?key=dZ5BlNRNhnRnPRnSsYHD3rLpSg7UFuY9")
+fetch("https://api.tomtom.com/search/2/search/" + urlArray +".json?key=dZ5BlNRNhnRnPRnSsYHD3rLpSg7UFuY9")
 .then (function (response) {
   return response.json();
 })
 .then (function (data) {
-  console.log(data);
+  // console.log(data);
+  var lat = data.results[0].position.lat;
+  var lon = data.results[0].position.lon;
+  restaurants(lat, lon);
+  console.log(lat, lon);
 })
+}
+
+function restaurants(lat, lon) {
+  fetch("https://api.tomtom.com/search/2/nearbySearch/.json?lat="+lat+"&lon="+lon+"&limit=20&radius=5000&categorySet=7315&view=Unified&key=dZ5BlNRNhnRnPRnSsYHD3rLpSg7UFuY9")
+  .then (function (response) {
+    return response.json();
+  })
+  .then (function (data) {
+    console.log(data);
+  })
 }
